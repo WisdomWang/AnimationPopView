37f31f6c8e6c2ee090c8e806d9f03e7d6e969e49
Your Name <you@example.com>
2018-07-18 15:33:25 +0800

    first commit

---
+++ AnimationPopViewDemo/CustomButton.swift
@@ -0,0 +1,21 @@
+//
+//  CustomButton.swift
+//  AnimationPopViewDemo
+//
+//  Created by Cary on 2018/7/16.
+//  Copyright © 2018年 Cary. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
